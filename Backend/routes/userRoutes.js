a4019a0f05721def870c79b24c3822f6f742067d
MuneneCalvin <munenecalvn@gmail.com>
2023-08-04 11:14:15 +0300

    Updated routes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -8,11 +8,11 @@ import {
 
 const router = Router();
 
-// READ
+
 router.get('/:id', verifyToken, getUser);
 router.get('/:id/friends', verifyToken, getUserFriends);
 
-// UPDATE
+
 router.patch('/:id/:friendId', verifyToken, addRemoveFriend);
 
 export default router;
